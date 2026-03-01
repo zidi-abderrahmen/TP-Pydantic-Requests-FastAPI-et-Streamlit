6b8147ed36a78237b2f64135e11cd79664d63c57
zidiabderrahmen <rhoumazaza@gmail.com>
2026-03-01 23:33:17 +0100

    Fixed code between model user and the main app

--- Pydantic/main.py
+++ Pydantic/main.py
@@ -15,3 +15,14 @@ user_data = {
 }
 
 user1 = User(**user_data)
+
+user = User(name = 'Ali', email = 'ali', account_id = -12)
+print(user)
+
+# Convert from Model to JSON
+user_json_str = user.model_dump_json()
+print(user_json_str)
+
+# Converting from JSON to Model
+json_str = {"name": "Ali", "email": "ali@gmail.com", "account_id": 1234}
+user = user.parse_raw(json_str)
