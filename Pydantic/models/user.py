3be5100fd15bfbdc902c6c26b2b5752b865aff2f
zidiabderrahmen <rhoumazaza@gmail.com>
2026-03-01 23:32:15 +0100

    Updated the model user.py to convert from JSON to Model

--- Pydantic/models/user.py
+++ Pydantic/models/user.py
@@ -15,5 +15,10 @@ def validate_account_id(cls, value):
 user = User(name = 'Ali', email = 'ali', account_id = -12)
 print(user)
 
+# Convert from Model to JSON
 user_json_str = user.model_dump_json()
 print(user_json_str)
+
+# Converting from JSON to Model
+json_str = {"name": "Ali", "email": "ali@gmail.com", "account_id": 1234}
+user = user.parse_raw(json_str)
