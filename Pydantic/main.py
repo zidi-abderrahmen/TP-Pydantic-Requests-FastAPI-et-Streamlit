c50e8d27f1380792995ace0f2aaf6f08d4115871
zidiabderrahmen <rhoumazaza@gmail.com>
2026-03-01 23:22:22 +0100

    Updated main.py and Added unpacking a dictionary

--- Pydantic/main.py
+++ Pydantic/main.py
@@ -1,7 +1,17 @@
 from models.user import User
 
-user = User(
+user0 = User(
     name = "Salah",
     email = "salah@gmail.com",
     account_id = 12345
 )
+
+# Or unpacking a dictionary
+
+user_data = {
+    'name': 'Salah',
+    'email': 'salah@gmail.com',
+    'account_id': 12345
+}
+
+user1 = User(**user_data)
