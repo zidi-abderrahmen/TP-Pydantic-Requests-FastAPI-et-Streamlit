671d0f29e0a55c50dc20c8f5bdc2c9ad2ec98a8d
zidiabderrahmen <rhoumazaza@gmail.com>
2026-03-02 21:23:59 +0100

    Added the 'items/{item_id}' endpoint to print specific item

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -7,7 +7,12 @@ items = []
 def root():
     return {"Hello": "World"}
 
-@app.get("/items")
+@app.get("items")
 def create_item(item: str):
     items.append(item)
+    return item
+
+@app.get("items/{item_id}")
+def get_item(item_id: int) -> str:
+    item = items[item_id]
     return item
