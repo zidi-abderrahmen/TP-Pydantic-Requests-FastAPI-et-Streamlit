b2004660b8d066896dc34c76eed2dd24fe13160c
zidiabderrahmen <rhoumazaza@gmail.com>
2026-03-02 22:00:40 +0100

    Added bar/line chart with numpy library

--- Streamlit/main.py
+++ Streamlit/main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 x = st.text_input("Favorite Movie?")
 
@@ -11,3 +12,11 @@ st.title(f"Your favorite movie is {x}")
 data = pd.read_csv("movies.csv")
 
 st.write(data)
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=["a", "b", "c"]
+)
+
+st.bar_chart(chart_data)
+st.line_chart(chart_data)
