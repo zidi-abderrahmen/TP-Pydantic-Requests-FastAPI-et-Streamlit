9d057d43890abf69d79d26f8cac70c129d8ed0c4
zidiabderrahmen <rhoumazaza@gmail.com>
2026-03-02 21:26:43 +0100

    Updated the 'items/{item_id}' enpoint

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -13,6 +13,8 @@ def create_item(item: str):
     return item
 
 @app.get("items/{item_id}")
-def get_item(item_id: int) -> str:
-    item = items[item_id]
-    return item
+def get_item(item_id: int) -> None:
+    if item_id < len(items):
+        return items[item_id]
+    else:
+        raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
