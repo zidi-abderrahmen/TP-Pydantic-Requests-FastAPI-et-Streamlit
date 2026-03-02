9d9a2ce484c21353c0ec95cafa3e52e2f33f4219
zidiabderrahmen <rhoumazaza@gmail.com>
2026-03-02 21:21:36 +0100

    Added the 'items' endpoint

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+items = []
 @app.get("/")
 def root():
     return {"Hello": "World"}
+
+@app.get("/items")
+def create_item(item: str):
+    items.append(item)
+    return item
