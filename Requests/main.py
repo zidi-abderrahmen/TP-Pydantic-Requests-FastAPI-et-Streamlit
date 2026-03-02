08714a780fbd9e2bf575f0ea722523e2f82563a9
zidiabderrahmen <rhoumazaza@gmail.com>
2026-03-02 20:44:40 +0100

    Added an example of Web Scraping with BeautifulSoup

--- Requests/main.py
+++ Requests/main.py
@@ -6,3 +6,9 @@ url = "https://www.example.com"
 response = requests.get(url)
 
 soup = BeautifulSoup(response.content, "html.parser")
+
+title = soup.title.text
+content = soup.find("p").text
+links = [a["href"] for a in soup.find_all("a")]
+
+print(title, content, links)
