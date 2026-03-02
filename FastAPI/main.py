ab71499609d19ec1658b89c5e9146a49ee881c04
zidiabderrahmen <rhoumazaza@gmail.com>
2026-03-02 21:31:43 +0100

    Added new class called Item and Updated the two function 'create_item' and 'get_item'

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    text: str = None
+    is_done: bool = False
 
 app = FastAPI()
 
@@ -8,12 +13,12 @@ def root():
     return {"Hello": "World"}
 
 @app.get("items")
-def create_item(item: str):
+def create_item(item: Item):
     items.append(item)
     return item
 
 @app.get("items/{item_id}")
-def get_item(item_id: int) -> None:
+def get_item(item_id: int) -> Item:
     if item_id < len(items):
         return items[item_id]
     else:
