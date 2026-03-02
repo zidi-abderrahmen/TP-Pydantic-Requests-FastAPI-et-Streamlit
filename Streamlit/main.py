12733b7253586ee2273fc57acfeff917f2bc7505
zidiabderrahmen <rhoumazaza@gmail.com>
2026-03-02 21:59:25 +0100

    Added movie list in an excel file called 'movies.csv' and use it with pandas library

--- Streamlit/main.py
+++ Streamlit/main.py
@@ -1,7 +1,13 @@
 import streamlit as st
+import pandas as pd
 
 x = st.text_input("Favorite Movie?")
 
 is_clicked = st.button("Click Me")
 
 st.title(f"Your favorite movie is {x}")
+
+
+data = pd.read_csv("movies.csv")
+
+st.write(data)
